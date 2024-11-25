05503a5db0663c9254bd1e8b75e7d12b5757ae29
eliolias <eliasv85@gmail.com>
2024-11-25 16:03:40 -0500

    added attitash logo url

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,15 @@
 import Lifts from "../Lifts";
 import Trails from "../Trails";
 import Weather from "../Weather/Weather";
-// import { resorts } from "../../assets/resorts";
+import { resorts } from "../../assets/resorts";
 import "./Card.css";
 
 function Card() {
   return (
     <>
-      {/* <h1>{resorts.wildcat.name}</h1> */}
       <header>
         <img
-          src="https://www.skiwildcat.com/Assets/images/sites/wildcatmountain/resort-logo-color.svg"
+          src={resorts.attitash.logo}
           alt="Wildcat Mountain logo"
         ></img>
       </header>
