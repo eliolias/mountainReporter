34bb31cfb02b717c5862669f7c3658f31f23514d
eliolias <65453595+eliolias@users.noreply.github.com>
2024-12-11 17:48:10 -0500

    made search responsive, need to fix to use name instead of key

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,22 +4,40 @@ import { resorts } from "../assets/resortsData";
 const Search = () => {
   const [searchInput, setSearchInput] = useState("");
   const [filteredResorts, setFilteredResorts] = useState(Object.keys(resorts));
+  const [clickedIn, setClickedIn] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     setSearchInput(event.target.value);
   };
 
+  // const showElements = useCallback(() => {
+  //   clickedIn = true;
+  //   console.log("INSIDE showElements function: " + clickedIn);
+  //   return clickedIn;
+  // });
+
+  // console.log("OUTSIDE & AFTER showElements function: " + clickedIn);
+
+  //NEED TO FIX TO USE name PROPERTY INSTEAD OF key
   useEffect(() => {
     if (searchInput.length > 0) {
       const filtered = Object.keys(resorts).filter((resort) =>
         resort.toLowerCase().includes(searchInput.toLowerCase())
       );
       setFilteredResorts(filtered);
+      console.log(filteredResorts);
+      setClickedIn(true);
     } else {
       setFilteredResorts(Object.keys(resorts));
+      setClickedIn(true);
     }
   }, [searchInput]);
+
+  // useEffect(() => {
+  //   showElements();
+  //   console.log("INSIDE USE EFFECT: " + clickedIn);
+  // }, [clickedIn, showElements]);
 
   return (
     <>
@@ -30,7 +48,7 @@ const Search = () => {
             id="search"
             type="text"
             placeholder="  Search for your mountain"
-            onClick={showElements}
+            // onClick={showElements}
             onChange={handleChange}
             value={searchInput}
           />
@@ -38,9 +56,11 @@ const Search = () => {
       </div>
       <div className="results-container">
         <ul className="results-list" id="list">
-          {filteredResorts.map((resort: string) => {
-            return <h3 key={resort}>{resort}</h3>;
-          })}
+          {clickedIn
+            ? filteredResorts.map((resort: string) => {
+                return <h3 key={resort}>{resort}</h3>;
+              })
+            : null}
         </ul>
       </div>
     </>
